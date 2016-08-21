514b1d0508019935d792d50b9a9dac11f6de0e5f
crestAT <a.schmidhuber@gmail.com>
2016-08-21 18:39:25 +0200

    v0.6-b1
    devel download source

--- extended-gui_install.php
+++ extended-gui_install.php
@@ -2,12 +2,14 @@
 <?php
 require_once("config.inc");
 
+$branch = "development";    // GIT branch: master, development
+$min_release = 10.3032853;
+
 // check FreeBSD release for fetch options >= 9.3
 $release = explode("-", exec("uname -r"));
 if ($release[0] >= 9.3) $verify_hostname = "--no-verify-hostname";
 else $verify_hostname = "";
 
-$min_release = 10.3032853;
 // create FreeBSD $current_release for min_release check
 $product_version = explode(".", get_product_version());                 // p.version = 10.3.0.3, p.revision = 2853
 $current_release = $product_version[0].".".$product_version[1].$product_version[2].$product_version[3].get_product_revision();
@@ -19,7 +21,7 @@ if ($current_release < floatval($min_release)) {                        // relea
 $dirname = dirname(__FILE__);
 if (!is_dir("{$dirname}/extended-gui/backup")) { mkdir("{$dirname}/extended-gui/backup", 0775, true); }
 if (!is_dir("{$dirname}/extended-gui/log")) { mkdir("{$dirname}/extended-gui/log", 0775, true); }
-$return_val = mwexec("fetch {$verify_hostname} -vo {$dirname}/extended-gui/extended-gui-install.php 'https://raw.github.com/crestAT/nas4free-extended-gui/master/extended-gui/extended-gui-install.php'", true);
+$return_val = mwexec("fetch {$verify_hostname} -vo {$dirname}/extended-gui/extended-gui-install.php https://raw.github.com/crestAT/nas4free-extended-gui/{$branch}/extended-gui/extended-gui-install.php", true);
 if ($return_val == 0) { 
     chmod("{$dirname}/extended-gui/extended-gui-install.php", 0775);
     require_once("{$dirname}/extended-gui/extended-gui-install.php"); 
