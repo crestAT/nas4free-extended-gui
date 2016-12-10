1b651c4b92b8865daafabbdaaab0cb9d9ad4f6c1
crestAT <a.schmidhuber@gmail.com>
2016-12-10 11:10:43 +0100

    v0.6-final

--- extended-gui/files/graph_index.php
+++ extended-gui/files/graph_index.php
@@ -39,8 +39,8 @@ require("guiconfig.inc");
 header("Content-type: image/svg+xml");
 
 $config_file = "ext/extended-gui/extended-gui.conf";
-require_once("ext/extended-gui/json.inc");
-if (($configuration = load_config($config_file)) === false) $input_errors[] = sprintf(gettext("Configuration file %s not found!"), "extended-gui.conf");
+require_once("ext/extended-gui/extension-lib.inc");
+if (($configuration = ext_load_config($config_file)) === false) $input_errors[] = sprintf(gettext("Configuration file %s not found!"), "extended-gui.conf");
 
 /********** HTTP GET Based Conf ***********/
 $ifnum=@htmlspecialchars($_GET["ifnum"]);  // BSD / SNMP interface name / number
@@ -100,7 +100,6 @@ echo "<?xml version=\"1.0\" encoding=\"{$encoding}\"?>\n";
     <text id="graph_out_lbl" x="3" y="13" <?=$attribs['out']?>><?=gettext("Out");?> <tspan id="graph_out_txt" <?=$attribs['out']?>> </tspan></text>
     <text id="switch_unit" x="<?=$width*0.60?>" y="5" <?=$attribs['switch_unit']?>><?=sprintf(gettext("Switch to %s/s"), ("bits" === $unit) ? "bytes" : "bits");?></text>
     <text id="switch_scale" x="<?=$width*0.60?>" y="11" <?=$attribs['switch_scale']?>><?=gettext("AutoScale");?> (<?=("up" === $scale_type) ? gettext("Up") : gettext("Follow");?>)</text>
-    <text id="datetime" x="<?=$width*0.40?>" y="5" <?=$attribs['legend']?>> </text>
     <text id="interface_name"  x="<?=$width*0.99?>" y="7" <?=$attribs['in']?> text-anchor="end"><?=$ifname?></text>
     <polygon id="axis_arrow_x" <?=$attribs['axis']?> points="<?=($width) . "," . ($height)?> <?=($width-2) . "," . ($height-2)?> <?=($width-2) . "," . $height?>"/>
     <text id="error" x="<?=$width*0.5?>" y="<?=$height*0.4?>" visibility="hidden" <?=$attribs['error']?> text-anchor="middle"><?=$error_text?></text>
@@ -196,11 +195,6 @@ function fetch_data() {
 }
 
 function plot_data(obj) {
-  // Show datetimelegend
-  var now = new Date();
-  var datetime = (now.getMonth()+1) + "/" + now.getDate() + "/" + now.getFullYear() + ' ' +
-    formatString(now.getHours()) + ":" + formatString(now.getMinutes()) + ":" + formatString(now.getSeconds());
-  SVGDoc.getElementById('datetime').firstChild.data = datetime;
 
   if (!obj.success)
     return handle_error();  // getURL failed to get data
--- extended-gui_install.php
+++ extended-gui_install.php
@@ -1,8 +1,42 @@
 #!/usr/local/bin/php-cgi -f
 <?php
+/*
+    extended-gui_install.php
+
+    Copyright (c) 2014 - 2017 Andreas Schmidhuber <info@a3s.at>
+    All rights reserved.
+
+	Portions of NAS4Free (http://www.nas4free.org).
+	Copyright (c) 2012-2016 The NAS4Free Project <info@nas4free.org>.
+	All rights reserved.
+
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions are met:
+
+    1. Redistributions of source code must retain the above copyright notice, this
+       list of conditions and the following disclaimer.
+    2. Redistributions in binary form must reproduce the above copyright notice,
+       this list of conditions and the following disclaimer in the documentation
+       and/or other materials provided with the distribution.
+
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+    ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+    WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+    DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+    ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+    (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+    LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+    ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+    (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+    SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+    The views and conclusions contained in the software and documentation are those
+    of the authors and should not be interpreted as representing official policies,
+    either expressed or implied, of the FreeBSD Project.
+*/
 require_once("config.inc");
 
-$branch = "development";    // GIT branch: master, development
+$branch = "master";    // GIT branch: master, development
 $min_release = 10.3032853;
 
 // check FreeBSD release for fetch options >= 9.3
@@ -21,7 +55,7 @@ if ($current_release < floatval($min_release)) {                        // relea
 $dirname = dirname(__FILE__);
 if (!is_dir("{$dirname}/extended-gui/backup")) { mkdir("{$dirname}/extended-gui/backup", 0775, true); }
 if (!is_dir("{$dirname}/extended-gui/log")) { mkdir("{$dirname}/extended-gui/log", 0775, true); }
-$return_val = mwexec("fetch {$verify_hostname} -vo {$dirname}/extended-gui/extended-gui-install.php https://raw.github.com/crestAT/nas4free-extended-gui/{$branch}/extended-gui/extended-gui-install.php", true);
+$return_val = mwexec("fetch {$verify_hostname} -vo {$dirname}/extended-gui/extended-gui-install.php https://raw.github.com/crestAT/nas4free-extended-gui/{$branch}/extended-gui/extended-gui-install.php", false);
 if ($return_val == 0) { 
     chmod("{$dirname}/extended-gui/extended-gui-install.php", 0775);
     require_once("{$dirname}/extended-gui/extended-gui-install.php"); 
